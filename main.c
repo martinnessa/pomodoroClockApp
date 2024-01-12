ab19aa05f7fd4de4dbaf0ccb1515b8d6070f4b63
Martin Nessa <martinnessa7@gmail.com>
2024-01-12 23:43:36 +0100

    Added time and calendar to app

--- main.c
+++ main.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
+#include <time.h>
 
 int main(){
-  printf("Welcome to the Pomodoro Clock App\n");
+
+	time_t currentTime = time(NULL);
+	struct tm *cur_time = localtime(&currentTime);
+
+	char calendarBuffer[100];
+	char timeBuffer[50];
+	
+	int displayCalendarInfo = strftime(calendarBuffer, 100,"%A %d %B %Y", cur_time);
+	int displayTimeInfo = strftime(timeBuffer, 50, "Current time: %H:%M:%S", cur_time);
+
+	printf("Welcome to the Pomodoro Clock App!\n");
+	printf("%s\n", calendarBuffer);
+	printf("%s\n", timeBuffer);
 }
